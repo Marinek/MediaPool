aef7784c3ac72f0f2d62f6ddf986c9646ad9db33
mm20482 <mm20482@4ebfd506-c8e9-8f2f-c4b0-7101dc0c4d68>
2011-12-18 14:34:44 +0000

    refactored to core

--- source/core-api/src/de/mediapool/core/persistence/dao/interfaces/IMediaDAO.java
+++ source/core-api/src/de/mediapool/core/persistence/dao/interfaces/IMediaDAO.java
@@ -3,7 +3,7 @@ package de.mediapool.core.persistence.dao.interfaces;
 import java.util.List;
 
 import de.mediapool.core.persistence.core.interfaces.IDataAccessObject;
-import de.mediapool.core.persistence.dao.media.MediaVO;
+import de.mediapool.core.persistence.vo.media.MediaVO;
 
 
 public interface IMediaDAO<T extends MediaVO> extends IDataAccessObject<T> {
--- source/core-api/src/de/mediapool/core/persistence/dao/interfaces/IMovieDAO.java
+++ source/core-api/src/de/mediapool/core/persistence/dao/interfaces/IMovieDAO.java
@@ -1,6 +1,6 @@
 package de.mediapool.core.persistence.dao.interfaces;
 
-import de.mediapool.core.persistence.dao.media.MovieVO;
+import de.mediapool.core.persistence.vo.media.MovieVO;
 
 public interface IMovieDAO<T extends MovieVO> extends IMediaDAO<T> {
 
