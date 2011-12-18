c8e513a23abe780ed33cd84843cad813b8d84cf4
mm20482 <mm20482@4ebfd506-c8e9-8f2f-c4b0-7101dc0c4d68>
2011-12-18 15:15:38 +0000

    get

--- trunk/source/core-api/src/de/mediapool/core/services/interfaces/IMediaService.java
+++ trunk/source/core-api/src/de/mediapool/core/services/interfaces/IMediaService.java
@@ -1,7 +1,10 @@
 package de.mediapool.core.services.interfaces;
 
+import java.util.List;
+
 import de.mediapool.core.beans.media.AbstractMediaBean;
 import de.mediapool.core.exceptions.MPExeption;
+import de.mediapool.core.persistence.vo.media.MediaVO;
 
 
 
@@ -9,4 +12,6 @@ public interface IMediaService extends IService {
 	
 	public AbstractMediaBean createMedia() throws MPExeption;
 	public void deleteMedia() throws MPExeption;
+	public void getAllMedia() throws MPExeption;
+	public void getMedia(int id) throws MPExeption;
 }
