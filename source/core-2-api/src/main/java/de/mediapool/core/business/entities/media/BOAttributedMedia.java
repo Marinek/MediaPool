d0a53f1c3496bc0e304db843b1ae8fc751ad9c51
martin.gasse <Marinek@users.noreply.github.com>
2012-07-10 20:07:37 +0000

    removed protected Constructors.

--- source/core-2-api/src/main/java/de/mediapool/core/business/entities/media/BOAttributedMedia.java
+++ source/core-2-api/src/main/java/de/mediapool/core/business/entities/media/BOAttributedMedia.java
@@ -9,20 +9,12 @@ import de.mediapool.core.exceptions.MPExeption;
 
 public class BOAttributedMedia extends BOAbstractEntity<AttributedMediaBean> {
 
-	protected BOAttributedMedia(UserBean pUserBean) throws MPExeption {
+	public BOAttributedMedia(UserBean pUserBean) throws MPExeption {
 		super(pUserBean);
 	}
 
-	protected BOAttributedMedia(UUID mediaID, UserBean pUserBean) throws MPExeption {
+	public BOAttributedMedia(UUID mediaID, UserBean pUserBean) throws MPExeption {
 		super(mediaID, pUserBean);
-	}
-
-	public static BOAttributedMedia getInstance(UserBean pUserBean) throws MPExeption {
-		return new BOAttributedMedia(pUserBean);
-	}
-
-	public static BOAttributedMedia getInstance(UUID mediaID, UserBean pUserBean) throws MPExeption {
-		return new BOAttributedMedia(mediaID, pUserBean);
 	}
 
 	protected void protectedSave() throws MPExeption {
