b58a014aadc90d0908a0bd968eb0f9eb404d5592
mm20482 <mm20482@4ebfd506-c8e9-8f2f-c4b0-7101dc0c4d68>
2012-10-28 18:12:49 +0000

    webservice spring

--- source/mediapool-webservice/src/main/java/de/mediapool/webservice/MobileService.java
+++ source/mediapool-webservice/src/main/java/de/mediapool/webservice/MobileService.java
@@ -1,13 +1,44 @@
 package de.mediapool.webservice;
 
+import de.mediapool.core.beans.business.entity.product.ProductBean;
+import de.mediapool.core.exceptions.MPExeption;
+import de.mediapool.core.services.MPLocalService;
+import de.mediapool.core.services.interfaces.IProductService;
+import de.mediapool.core.services.product.ProductServiceImpl;
+
 public class MobileService {
 
-	public String sayHello(String name) {
+	private ProductServiceImpl productService;
+
+	public String sayHello(String name) throws MPExeption {
+		IProductService productService = MPLocalService.getInstance().getProductService();
+		ProductBean pb = productService.getProduct(null, null);
+		// ProductMediaResultList pmList =
+		// productService.getProductByEan("12345677", null);
+		StringBuffer str = new StringBuffer();
+		str.append("ean ");
+		str.append(pb.getAttribute("ean"));
+		// for (ProductMediaBean lBean : pmList) {
+		// for (EntityAttributeValueBean lValueBean : lBean.getAttributes()) {
+		// if (lValueBean.getAttributeVisible()) {
+		// str.append(lValueBean.getAttributeValue());
+		// }
+		// }
+		// }
+
 		if (name == null) {
-			return "Hello";
+			return str.toString();
 		}
 
 		return "Hello, " + name + "!";
+	}
+
+	public ProductServiceImpl getProductService() {
+		return productService;
+	}
+
+	public void setProductService(ProductServiceImpl productService) {
+		this.productService = productService;
 	}
 
 }
