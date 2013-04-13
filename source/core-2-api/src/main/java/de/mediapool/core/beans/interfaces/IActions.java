590098cdec603479150d0301aabb610770fad3c3
stefan.braun.87 <stefan.braun.87@googlemail.com>
2013-04-13 08:53:44 +0000

    Interface für ActionCommands und Displayedtext in Actions hinzugefügt

---
+++ source/core-2-api/src/main/java/de/mediapool/core/beans/interfaces/IActions.java
@@ -0,0 +1,13 @@
+package de.mediapool.core.beans.interfaces;
+
+public class IActions {
+	
+	public static final String ACTION_OPEN = "open";
+	public static final String ACTION_SAVE = "save";
+	public static final String ACTION_ADD = "add";
+	
+	public static final String DISPLAYED_TEXT_OPEN = "Öffnen";
+	public static final String DISPLAYED_TEXT_SAVE = "Speichern";
+	public static final String DISPLAYED_TEXT_ADD = "Hinzufügen";
+
+}
