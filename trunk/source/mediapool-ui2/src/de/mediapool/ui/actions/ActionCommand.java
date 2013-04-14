6d9fff2230c75911dfd383077910cb905c9f282d
martin.gasse@gmail.com <martin.gasse@gmail.com@4ebfd506-c8e9-8f2f-c4b0-7101dc0c4d68>
2013-04-14 07:05:52 +0000

    changed action commands to be stateless.

--- trunk/source/mediapool-ui2/src/de/mediapool/ui/actions/ActionCommand.java
+++ trunk/source/mediapool-ui2/src/de/mediapool/ui/actions/ActionCommand.java
@@ -3,7 +3,8 @@ package de.mediapool.ui.actions;
 import com.vaadin.data.util.BeanItem;
 
 public interface ActionCommand {
+
 	public void execute(BeanItem<?> lBeanItem);
-	public boolean isActionCommandFor(String pActionCommand);
+
 	public String getActionCommand();
 }
---
+++ trunk/source/mediapool-ui2/src/de/mediapool/ui/actions/commands/EmptyActionCommand.java
@@ -0,0 +1,56 @@
+package de.mediapool.ui.actions.commands;
+
+import com.vaadin.data.util.BeanItem;
+
+import de.mediapool.ui.actions.ActionCommand;
+
+public class EmptyActionCommand implements ActionCommand {
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// Statische Deklarationen
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	public static final String NAME = "EmptyAction";
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// Member Variablen
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	private String actionCommand;
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// Konstruktoren
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	public EmptyActionCommand() {
+	}
+
+	public EmptyActionCommand(String pActionCommand) {
+		this.actionCommand = pActionCommand;
+	}
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// public Methoden
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	public void execute(BeanItem<?> pBeanItem) {
+		System.err.println("No Action named '" + this.actionCommand + "' defined for Bean " + pBeanItem);
+	}
+
+	@Override
+	public String getActionCommand() {
+		return "EmptyAction";
+	}
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// protected Methoden
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// private Methoden
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+	// abstrakte Methoden
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+}
