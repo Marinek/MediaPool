c00131b70eabe1bf8f9d0f9401106b7a17ffa4dd
martin.gasse <Marinek@users.noreply.github.com>
2013-04-14 07:05:52 +0000

    changed action commands to be stateless.

--- source/mediapool-ui2/src/de/mediapool/ui/actions/ActionCommandManager.java
+++ source/mediapool-ui2/src/de/mediapool/ui/actions/ActionCommandManager.java
@@ -1,38 +1,49 @@
 package de.mediapool.ui.actions;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import de.mediapool.ui.actions.commands.EmptyActionCommand;
 import de.mediapool.ui.actions.commands.SaveActionCommand;
 
-
-public class ActionCommandManager{
+public class ActionCommandManager {
 
 	private static ActionCommandManager manager = null;
-	private Map<String,ActionCommand> actionCommands = null;
-	
-	private ActionCommandManager(){
+	private Map<String, Class<? extends ActionCommand>> actionCommands = new HashMap<String, Class<? extends ActionCommand>>();
+
+	private ActionCommandManager() {
 		this.init();
 	}
-	public static ActionCommandManager getCommandManagerInstance(){
-		if(manager == null){
+
+	public static ActionCommandManager getCommandManagerInstance() {
+		if (manager == null) {
 			manager = new ActionCommandManager();
 		}
 		return manager;
 	}
+
 	/**
 	 * Funktion zum initialen Hinzufügen von Actionsklassen
 	 */
 	private void init() {
-		this.actionCommands= new HashMap<String,ActionCommand>();
-		this.actionCommands.put(SaveActionCommand.ACTION_COMMAND,new SaveActionCommand());
+		this.actionCommands.put(SaveActionCommand.ACTION_COMMAND, SaveActionCommand.class);
 	}
-	
-	public ActionCommand getActionCommandFor(String pActionCommand){
-		if(this.actionCommands.keySet().contains(pActionCommand)){
-		 return this.actionCommands.get(pActionCommand);
+
+	public ActionCommand getActionCommandFor(String pActionCommand) {
+		if (this.actionCommands.keySet().contains(pActionCommand)) {
+			return this.activate(this.actionCommands.get(pActionCommand));
 		}
+		return new EmptyActionCommand();
+	}
+
+	private ActionCommand activate(Class<? extends ActionCommand> actionCommandClass) {
+		try {
+			return actionCommandClass.newInstance();
+		} catch (Throwable e) {
+			// TODO: Exception
+		}
+		// TODO: Exception!!!
 		return null;
+
 	}
 }
