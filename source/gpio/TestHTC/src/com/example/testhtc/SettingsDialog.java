0a9da34b787fecdae3967f9c6b97a796f2a74f4c
mm20482 <mm20482@googlemail.com>
2014-01-11 17:40:19 +0000

    setinngsmenu with persistence

---
+++ source/gpio/TestHTC/src/com/example/testhtc/SettingsDialog.java
@@ -0,0 +1,110 @@
+package com.example.testhtc;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SettingsDialog extends Dialog implements OnClickListener {
+
+	private Button cancel_Button;
+	private Button backup_Button;
+	private Button save_Button;
+	private Context context;
+	private SharedPreferences prefs;
+	private String urlString = "com.example.testhtc.url";
+	private String refreshString = "com.example.testhtc.refresh";
+
+	public SettingsDialog(Context context) {
+		super(context);
+		this.context = context;
+		setContentView(R.layout.settings);
+		prefs = context.getSharedPreferences("com.example.testhtc", Context.MODE_PRIVATE);
+		getWindow().setLayout(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT);
+
+		initButtons();
+	}
+
+	@Override
+	public void onClick(View v) {
+		int button_id = v.getId();
+		Log.w("settingsbutton", button_id + "");
+		switch (button_id) {
+		case (R.id.settings_save_button):
+			saveSettings();
+			break;
+		case (R.id.settings_backup_button):
+			resetSettings();
+			break;
+		default:
+			this.cancel();
+		}
+
+	}
+
+	private void saveSettings() {
+
+		String new_url = ((TextView) findViewById(R.id.settings_url_text)).getText().toString();
+		String new_refreshTime = ((TextView) findViewById(R.id.settings_refreshtime_text)).getText().toString();
+		changeSettings(new_refreshTime, new_url);
+
+		printAsToast("Einstellungen gespeichert");
+
+		this.cancel();
+	}
+
+	private void resetSettings() {
+
+		String default_url = context.getResources().getString(R.string.settings_url_text);
+		String default_refreshTime = context.getResources().getString(R.string.settings_refreshtime_text);
+		changeSettings(default_refreshTime, default_url);
+
+		printAsToast("Einstellungen zurückgesetzt");
+		this.cancel();
+	}
+
+	private void changeSettings(String refreshTime, String url) {
+		SharedPreferences.Editor editor = prefs.edit();
+
+		TextView url_text = (TextView) findViewById(R.id.settings_url_text);
+		url_text.setText(url);
+
+		TextView refreshTime_text = (TextView) findViewById(R.id.settings_refreshtime_text);
+		refreshTime_text.setText(refreshTime);
+
+		editor.putString(urlString, url_text.getText().toString());
+		editor.putString(refreshString, refreshTime_text.getText().toString());
+
+		editor.commit();
+
+	}
+
+	private void printAsToast(String message) {
+		int duration = Toast.LENGTH_SHORT;
+		Log.w("Toast", message.toString());
+		Toast toast = Toast.makeText(context, message, duration);
+		toast.show();
+	}
+
+	private void initButtons() {
+		cancel_Button = (Button) findViewById(R.id.settings_cancel_button);
+		backup_Button = (Button) findViewById(R.id.settings_backup_button);
+		save_Button = (Button) findViewById(R.id.settings_save_button);
+
+		cancel_Button.setOnClickListener(this);
+		backup_Button.setOnClickListener(this);
+		save_Button.setOnClickListener(this);
+
+		TextView url_text = (TextView) findViewById(R.id.settings_url_text);
+		url_text.setText(prefs.getString(urlString, context.getResources().getString(R.string.settings_url_text)));
+		TextView refreshTime_text = (TextView) findViewById(R.id.settings_refreshtime_text);
+		refreshTime_text.setText(prefs.getString(refreshString, context.getResources().getString(R.string.settings_refreshtime_text)));
+
+	}
+}
